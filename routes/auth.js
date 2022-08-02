514bd2a753ed737065fa4478c0fb7d40d940ec34
KimSH39 <sooooooohyeon5@naver.com>
2022-08-02 13:32:46 +0900

    feat: 회원가입 구현

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,76 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/User.js')
 
-const passport = require("../passport/index.js");
+const { isNotLoggedIn } = require('../middlewares/auth')
 
-const router = express.Router();
+const passport = require('../passport/index.js')
 
-router.post("/signin", (req, res, next) => {
-  passport.authenticate("local", (authError, user, info) => {
-    console.info("___passport.authenticate()");
+const router = express.Router()
+
+router.post('/signup', isNotLoggedIn, async (req, res, next) => {
+  const { email, password, name, type, nickname, wallet } = req.body
+  const salt = 12
+
+  try {
+    const exUser = await User.findOne({ where: { email } })
+    if (exUser) {
+      return res.status(400).json({ error: '이미 존재하는 이메일입니다.' })
+    }
+    console.info('___User.create(): ' + name)
+    const hash = await bcrypt.hash(password, salt)
+    await User.create({
+      email,
+      password: hash,
+      name,
+      type,
+      nickname,
+      wallet,
+    })
+    return res.status(201).json({
+      message: '회원가입 성공',
+      data: {
+        id: nickname,
+        name: name,
+      },
+    })
+  } catch (error) {
+    console.error(error)
+    return next(error)
+  }
+})
+
+router.post('/signin', (req, res, next) => {
+  passport.authenticate('local', (authError, user, info) => {
+    console.info('___passport.authenticate()')
     if (authError) {
-      console.error(authError);
-      return next(authError);
+      console.error(authError)
+      return next(authError)
     }
     if (!user) {
       return res
         .status(400)
-        .json({ message: `로그인 실패 - ${info.message}`, data: {} });
+        .json({ message: `로그인 실패 - ${info.message}`, data: {} })
     }
 
-    console.info("___req.login()");
+    console.info('___req.login()')
     return req.login(user, { session: false }, (loginError) => {
       if (loginError) {
-        console.error(loginError);
-        return next(loginError);
+        console.error(loginError)
+        return next(loginError)
       }
 
       const token = jwt.sign(
         { id: user.id, name: user.name },
-        process.env.JWT_SECRET
-      );
+        process.env.JWT_SECRET,
+      )
       return res.json({
-        message: "로그인 성공",
+        message: '로그인 성공',
         data: { access_token: token },
-      });
-    });
-  })(req, res, next);
-});
+      })
+    })
+  })(req, res, next)
+})
 
-module.exports = router;
+module.exports = router
