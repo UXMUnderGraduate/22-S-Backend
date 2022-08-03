59612f5a99d483dd98b524081bcb26e4c8eacb45
KimSH39 <63898043+KimSH39@users.noreply.github.com>
2022-08-03 10:47:06 +0900

    feat: 회원가입 구현 (#20)
    * feat: 회원가입 구현
    
    * style: 코드 포맷팅 수정
    
    Co-authored-by: SeonjaeHyeon <hsj106@mju.ac.kr>

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/User.js");
+
+const { isNotLoggedIn } = require("../middlewares/auth");
 
 const passport = require("../passport/index.js");
 
 const router = express.Router();
+
+router.post("/signup", isNotLoggedIn, async (req, res, next) => {
+  const { email, password, name, type, nickname, wallet } = req.body;
+  const salt = 12;
+
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.status(400).json({ error: "이미 존재하는 이메일입니다." });
+    }
+    console.info("___User.create(): " + name);
+    const hash = await bcrypt.hash(password, salt);
+    await User.create({
+      email,
+      password: hash,
+      name,
+      type,
+      nickname,
+      wallet,
+    });
+    return res.status(201).json({
+      message: "회원가입 성공",
+      data: {
+        id: nickname,
+        name: name,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
 router.post("/signin", (req, res, next) => {
   passport.authenticate("local", (authError, user, info) => {
