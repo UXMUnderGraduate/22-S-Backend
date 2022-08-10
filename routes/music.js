02d3b786df18225161025e727ca919cd8cbe06cc
Yejin Kang <69359751+yejinie@users.noreply.github.com>
2022-08-10 20:45:31 +0900

    feat: 음원 상세 조회 기능 구현 (#35)
    * feat: 음원 상세 조회 기능 구현
    
    * feat: 음원 상세 조회 기능 구현
    
    * style: 코드 포맷팅 수정
    
    Co-authored-by: SeonjaeHyeon <hsj106@mju.ac.kr>

--- routes/music.js
+++ routes/music.js
@@ -2,8 +2,15 @@ const express = require("express");
 const { Op } = require("sequelize");
 const { isLoggedIn } = require("../middlewares/auth");
 const Music = require("../models/Music");
+const User = require("../models/User");
+const IPFS = require("ipfs-core");
 
 const router = express.Router();
+
+let node;
+(async () => {
+  node = await IPFS.create();
+})();
 
 router.get("/", isLoggedIn, async (req, res, next) => {
   const { search } = req.query;
@@ -85,6 +92,77 @@ router.get("/chart", isLoggedIn, async (req, res, next) => {
     console.error(err);
     return res.status(400).json({
       message: "음원 리스트 조회 실패",
+      data: {},
+    });
+  }
+});
+
+router.get("/:id", isLoggedIn, async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const music = await Music.findOne({
+      where: { id: id },
+      attributes: ["title", "genre", "artist", "cid1"],
+    });
+    const { title, genre, artist, cid1 } = music;
+
+    let chunks = [];
+    for await (const chunk of node.cat(cid1)) {
+      chunks.push(chunk);
+    }
+    const meta = Buffer.concat(chunks);
+
+    let songInfo = JSON.parse(meta).songInfo;
+    songInfo = JSON.stringify(songInfo);
+
+    const imageCid = JSON.parse(songInfo).imageCid;
+    chunks = [];
+    for await (const chunk of node.cat(imageCid)) {
+      chunks.push(chunk);
+    }
+    const image = Buffer.concat(chunks);
+    const encode = Buffer.from(image).toString("base64");
+
+    const composerId = JSON.parse(songInfo).composerId;
+    const composers = [];
+    for (let i in composerId) {
+      const composer = await User.findOne({
+        where: { id: composerId[i] },
+      });
+      composers.push(composer.nickname);
+    }
+
+    const songWriterId = JSON.parse(songInfo).songWriterId;
+    const songWriters = [];
+    for (let i in songWriterId) {
+      const songWriter = await User.findOne({
+        where: { id: songWriterId[i] },
+      });
+      songWriters.push(songWriter.nickname);
+    }
+
+    return res.json({
+      message: "음원 상세 조회 성공",
+      data: {
+        id: id,
+        title,
+        artistId: JSON.parse(songInfo).artistId,
+        artist,
+        album: JSON.parse(songInfo).album,
+        image: encode,
+        lyrics: JSON.parse(songInfo).lyrics,
+        genre,
+        composerId,
+        composer: composers,
+        songWriterId,
+        songWriter: songWriters,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({
+      message: "음원 상세 조회 실패",
       data: {},
     });
   }
