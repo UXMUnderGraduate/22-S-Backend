3db656c17c0cb7b271f8009a3e2c06c7ee60b13c
Seonjae Hyeon <hsj106@mju.ac.kr>
2022-08-11 11:12:03 +0900

    feat: 중복 곡 체크 기능 구현 (#36)
    * feat: 중복 곡 체크 기능 구현
    
    * fix: confidence 연산자 수정

--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { Op } = require("sequelize");
+const multer = require("multer");
+const axios = require("axios").default;
+const FormData = require("form-data");
 const { isLoggedIn } = require("../middlewares/auth");
 const Music = require("../models/Music");
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 
@@ -38,5 +42,52 @@ router.delete("/:id", isLoggedIn, async (req, res) => {
     });
   }
 });
+
+router.post(
+  "/check",
+  isLoggedIn,
+  upload.single("file"),
+  async (req, res, next) => {
+    const file = req.file;
+
+    const formData = new FormData();
+    formData.append("file", Buffer.from(file.buffer), file.originalname);
+
+    // dejavu 도커 이미지에서 flask 서버 포트를 7000번으로 열어야 함
+    const result = await axios.post(
+      "http://localhost:7000/recognize",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        maxBodyLength: Infinity,
+        maxContentLength: Infinity,
+      }
+    );
+
+    if (result.status !== 200) {
+      return res.status(400).json({
+        message: "중복 곡 체크 실패",
+        data: {},
+      });
+    }
+
+    let isDuplicated = false;
+    const { data } = result.data;
+    for (const song of data.results) {
+      // confidence 값이 0.3 이상인 경우 중복 곡으로 판단
+      if (song.input_confidence >= 0.3) {
+        isDuplicated = true;
+        break;
+      }
+    }
+
+    return res.json({
+      message: "중복 곡 체크 성공",
+      data: { isDuplicated },
+    });
+  }
+);
 
 module.exports = router;
