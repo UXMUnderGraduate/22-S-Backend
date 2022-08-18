a85d6d55e566b5188d615f544590e680b6175398
Seonjae Hyeon <hsj106@mju.ac.kr>
2022-08-18 13:23:59 +0900

    fix: IPFS LockExistsError 수정 (#46)
    * fix: IPFS 모듈 초기화 수정
    
    * feat: nodemon 재/시작 실행 커맨드 추가

--- modules/ipfs.js
+++ modules/ipfs.js
@@ -1,12 +1,19 @@
 const ipfs = require("ipfs-core");
 
 class IPFS {
-  constructor() {
-    this.init();
-  }
-
-  async init() {
-    this.node = await ipfs.create();
+  init() {
+    return new Promise((resolve, reject) => {
+      ipfs
+        .create()
+        .then((instance) => {
+          this.node = instance;
+          resolve();
+        })
+        .catch((err) => {
+          console.error(err);
+          reject(err);
+        });
+    });
   }
 
   getInstance() {
